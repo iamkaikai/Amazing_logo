0b952f1b35e466d3d22237a533db27b725303683
YENKAI HUANG <iamkaikai@livemail.tw>
2023-06-18 02:55:29 -0400

    init commit

--- parser.py
+++ parser.py
@@ -1,14 +1,19 @@
-import os
 import re
 
-file = logobook.html
+file = "logobook.html"
 
 with open(file, 'r') as f:
     content = f.read()
 
-pattern = r'http://www.logobook.com/[^"]*'
+# pattern = r'http://www\.logobook\.com/letter/([a-z]/|number-logos/all/)'
+pattern = r'(http://www\.logobook\.com/letter/[a-z]/|http://www\.logobook\.com/letter/number-logos/all/)'
+
+
+
 matches = re.findall(pattern, content)
 
 with open("links.txt", "w") as f:
     for link in matches:
         f.write(link + '\n')
+        
+print("saving file...")
