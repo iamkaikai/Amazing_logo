2cebd0866dcabfb997f70a741deea464c2d064a0
YENKAI HUANG <iamkaikai@livemail.tw>
2023-07-08 19:38:38 -0400

    remove duplicated import

--- data_gen.py
+++ data_gen.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 import pandas as pd
-from datasets import load_dataset
 
 dataset = load_dataset("imagefolder", data_dir="./amazing_logos/data", split="train")
 dataset.push_to_hub("iamkaikai/amazing_logos")
