4052e7cf4638196cde3de820e180be7234633189
Kyle Huang <iamkaikai@livemail.tw>
2023-07-20 15:09:29 -0400

    fixing grouping bug

--- svg2png.jsx
+++ svg2png.jsx
@@ -3,37 +3,36 @@ var inputFolder = Folder.selectDialog();
 var files = inputFolder.getFiles("*.svg");
 
 // create 'export' folder if it doesn't exist
-var outputFolder = new Folder(inputFolder.fsName + '/export');
+var outputFolder = new Folder(inputFolder.fsName + '/export_512b');
 if (!outputFolder.exists) {
     outputFolder.create();
 }
 
+var count = 0;
+var checkpoint = 0;
+var logFile = new File(outputFolder.fsName + '/log.txt');
+logFile.open('a'); // Open file for appending
+
 // loop through all files
 for (var i = 0; i < files.length; i++) {
-
-    var pngFileName = outputFolder.fsName + "/" + files[i].name.split(".")[0] + ".png";
-    var pngFile = new File(pngFileName);
     
     // Check if the file already exists, if so skip this iteration
-    if(!pngFile.exists){
-
-        var doc = app.open(files[i]); // open the document
-        var scaleFactor = Math.min(512 / doc.width, 512 / doc.height);
-    
-        // adjust the artboard size
+    if(count >= checkpoint){    
         try {
+            var fileName = files[i].name.substring(0, files[i].name.lastIndexOf('.'));
+            var pngFileName = outputFolder.fsName + "/" + fileName + ".png";
+            var pngFile = new File(pngFileName);
+            var doc = app.open(files[i]); // open the document
+            var scaleFactor = Math.min(512 / doc.width, 512 / doc.height);
             var width  = 512;
             var height = 512;
             var abBounds = doc.artboards[0].artboardRect; // left, top, right, bottom
-    
             var ableft   = abBounds[0];
             var abtop    = abBounds[1];
             var abwidth  = abBounds[2] - ableft;
-            var abheight = abtop- abBounds[3];
-    
+            var abheight = abtop - abBounds[3];
             var abctrx   = abwidth / 2 + ableft;
             var abctry   = abtop - abheight / 2;
-    
             var ableft   = abctrx - width  / 2;
             var abtop    = abctry + height / 2;
             var abright  = abctrx + width  / 2;
@@ -41,28 +40,32 @@ for (var i = 0; i < files.length; i++) {
     
             doc.artboards[0].artboardRect = [ableft, abtop, abright, abbottom];
             
-            // group all items in the document
-            // alert(doc.groupItems.length)
-            if (doc.groupItems.length <= 1){       // if no group
-                var group = doc.pageItems[0];   
-            }else{
-                var group = doc.groupItems[1];      // if there are groups
+
+            // put all elements in a group then resize it
+            var group = doc.groupItems.add();
+            itemSize = app.activeDocument.activeLayer.pageItems.length
+            for (var j = 0; j < itemSize; j++) {
+                doc.pageItems[j].move(group, ElementPlacement.INSIDE);
             }
-        
-            // resize the group
-            group.resize(scaleFactor * 80, scaleFactor * 80, true, true, true, true, scaleFactor * 140);
-            
+            group.resize(scaleFactor * 80, scaleFactor * 80, true, false, false, false, scaleFactor * 135);    
+
+
+            // save png
             var pngExportOptions = new ExportOptionsPNG24();
             pngExportOptions.transparency = false;
             pngExportOptions.artBoardClipping = true;
-        
-            
+                    
         }
         catch(e) {
-            alert(e.message);
+            logFile.writeln('Error with file: ' + files[i].name + ' at count: ' + i);
+            logFile.writeln('Error message: ' + e.message);
+            continue
         }
         doc.exportFile(pngFile, ExportType.PNG24, pngExportOptions);
         doc.close(SaveOptions.DONOTSAVECHANGES);
     }
+    count +=1
     
 }
+
+
